2af142c5150a1dfeb72d828472214222b6846dee
lattajadon <lattajadon@gmail.com>
2022-01-15 14:41:44 -0700

    first commit. testing git functionality

---
+++ android/src/com/nerdherd/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.nerdherd.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.nerdherd.game.NerdHerdGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new NerdHerdGame(), config);
+	}
+}
